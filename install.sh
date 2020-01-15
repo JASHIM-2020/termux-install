a93e54fcbe22ab5c38bb4ca69149abf24c7e6cd2
rajctg <39931753+jashim2018@users.noreply.github.com>
2020-01-15 23:55:25 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,19 +26,19 @@ apt install clang
 apt install hydra
 apt install gnupg
 apt install netcat
-apt install tmuxpt
 apt install cowsay
 apt install toilet
 apt install libbz2
-apt install tmuxpt
 apt install libc++
 apt install python
 apt install texinfo
 apt install libcurl
 apt install ncurses
 apt install openssl
+apt install openssl-tool
 apt install liblzma
 apt install python2
+apt install python-dev
 apt install libiconv
 apt install neofetch
 apt install dnsutils
@@ -49,7 +49,6 @@ apt install game-repo
 apt install util-linux
 apt install moon-buggy
 apt install libnghttp2
-apt install scienc-repo
 apt install termux-exec
 apt install openssl-tool
 apt install termux-tools
@@ -61,19 +60,24 @@ apt install termux-licenses
 apt install command-not-found
 apt install libandroid-support
 gem install lolcat
-pkg install update
-pkg install upgrade
+pkg update
+pkg upgrade
+pkg install root-repo
+pkg install bash
+pkg install libcurl
 pkg install php
 pkg install ruby
 pkg install python
 pkg install python2
 pkg install openssh
+pip insyall pint
 pip2 install bs4
 pip2 install rsa
 pip2 install gtts
 pip2 install scapy
 pip2 install fbchat
 pip2 install oauth2
+pip install pip tools
 pip2 install requests
 pip2 install colorama
 pip2 install selenium
@@ -81,12 +85,17 @@ pip2 install mechanize
 pip2 install dnspython
 pip2 install Whatportis
 pip2 install Flask-OAUth
-pip2 install googletrans
 pip install pinax-stripe
+pip2 install googletrans
+pip install ncurses-utils
 pip install pinax-messages
 pip2 install beautifulsoup4
 pip2 install django-social-auth
 pip2 install requests pexpect passlib
+
+
+
+
 
 
 
