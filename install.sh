9b3f133dd127251b48faa91b85ea5e9ec1932731
rajctg <39931753+jashim2018@users.noreply.github.com>
2020-01-16 22:18:55 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,99 +1,98 @@
-apt update
-apt upgrade
-apt install bc
-apt install apt
-apt install vim
-apt install zip
-apt install git
-apt install w3m
-apt install nano
-apt install ruby
-apt install curl
-apt install make
-apt install nmap
-apt install grep
-apt install perl
-apt install curl
-apt install make
-apt install gpgv
-apt install bash
-apt install dash
-apt install dpkg
-apt install zlib
-apt install wget
-apt install unzip
-apt install clang
-apt install hydra
-apt install gnupg
-apt install netcat
-apt install cowsay
-apt install toilet
-apt install libbz2
-apt install libc++
-apt install python
-apt install texinfo
-apt install libcurl
-apt install ncurses
-apt install openssl
-apt install openssl-tool
-apt install liblzma
-apt install python2
-apt install python-dev
-apt install libiconv
-apt install neofetch
-apt install dnsutils
-apt install readline
-apt install libgcrypt
-apt install termux-am
-apt install game-repo
-apt install util-linux
-apt install moon-buggy
-apt install libnghttp2
-apt install termux-exec
-apt install openssl-tool
-apt install termux-tools
-apt install libgpg-error
-apt install ncurses-utils
-apt install termux-keyring
-apt install ca-certificates
-apt install termux-licenses
-pkg install ncurses-utils
-apt install command-not-found
-apt install libandroid-support
-gem install lolcat
-pkg update
-pkg upgrade
-pkg install tsu
-pkg install root-repo
-pkg install bash
-pkg install libcurl
-pkg install php
-pkg install ruby
-pkg install python
-pkg install python2
-pkg install openssh
-pkg install figlet
-pip insyall pint
-pip2 install bs4
-pip2 install rsa
-pip2 install gtts
-pip2 install scapy
-pip2 install fbchat
-pip2 install oauth2
-pip2 install requests
-pip2 install colorama
-pip2 install selenium
-pip2 install mechanize
-pip2 install dnspython
-pip2 install Whatportis
-pip2 install Flask-OAUth
-pip install pinax-stripe
-pip2 install googletrans
-pip install ncurses-utils
-pip install pinax-messages
-pip2 install beautifulsoup4
-pip2 install django-social-auth
-pip2 install requests pexpect passlib
+apt update -y
+apt upgrade -y
+apt install bc -y
+apt install apt -y
+apt install vim -y
+apt install zip -y
+apt install git -y
+apt install w3m -y
+apt install nano -y
+apt install ruby -y
+apt install curl -y
+apt install make -y
+apt install nmap -y
+apt install grep -y
+apt install perl -y
+apt install curl -y
+apt install make -y
+apt install gpgv -y
+apt install bash -y
+apt install dash -y
+apt install dpkg -y
+apt install zlib -y
+apt install wget -y
+apt install unzip -y
+apt install clang -y
+apt install hydra -y
+apt install gnupg -y
+apt install netcat -y
+apt install cowsay -y
+apt install toilet -y
+apt install libbz2 -y
+apt install libc++ -y
+apt install python -y
+apt install texinfo -y
+apt install libcurl -y
+apt install ncurses -y
+apt install openssl -y
+apt install openssl-tool -y
+apt install liblzma -y
+apt install python2 -y
+apt install python-dev -y
+apt install libiconv -y
+apt install neofetch -y
+apt install dnsutils -y
+apt install readline -y
+apt install libgcrypt -y
+apt install termux-am -y
+apt install game-repo -y
+apt install util-linux -y
+apt install moon-buggy -y
+apt install libnghttp2 -y
+apt install termux-exec -y
+apt install openssl-tool -y
+apt install termux-tools -y
+apt install libgpg-error -y
+apt install ncurses-utils -y
+apt install termux-keyring -y
+apt install ca-certificates -y
+apt install termux-licenses -y
+pkg install ncurses-utils -y
+apt install command-not-found -y
+apt install libandroid-support-y
+gem install lolcat-y
+pkg update -y
+pkg upgrade -y
+pkg install tsu -y
+pkg install bash -y
+pkg install libcurl -y
+pkg install php -y
+pkg install ruby-y
+pkg install python -y
+pkg install python2 -y
+pkg install openssh -y
+pkg install figlet -y
+pip insyall pint -y
+pip2 install bs4 -y
+pip2 install rsa -y
+pip2 install gtts -y
+pip2 install scapy -y
+pip2 install fbchat -y
+pip2 install oauth2 -y
+pip2 install requests -y
+pip2 install colorama -y
+pip2 install selenium -y
+pip2 install mechanize -y
+pip2 install dnspython -y
+pip2 install Whatportis -y
+pip2 install Flask-OAUth -y
+pip install pinax-stripe -y
+pip2 install googletrans -y
+pip install ncurses-utils -y
+pip install pinax-messages -y
+pip2 install beautifulsoup4 -y
+pip2 install django-social-auth -y
+pip2 install requests pexpect passlib -y
 git clone https://gitlab.com/st42/termux-sudo
 cd termux-sudo
 cp sudo /data/data/com.termux/files/usr/bin
