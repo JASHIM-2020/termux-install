34e739e8d0eea0ba7728bcd26221012269e4dd9a
rajctg <39931753+jashim2018@users.noreply.github.com>
2020-01-17 18:36:47 +0600

    Update dark1.8.sh

--- dark1.8.sh
+++ dark1.8.sh
@@ -2,3 +2,4 @@ git clone https://github.com/jashim2018/dark1.8
 git clone https://github.com/jashim2018/dark1.8A
 git clone https://github.com/jashim2018/dark1.8eng
 git clone https://github.com/jashim2018/dark1.8iemfast
+git clone https://github.com/jashim2018/dark1.8p
