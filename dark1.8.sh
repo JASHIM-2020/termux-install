18288f5011cc17689b934c1ddf3ad3e49904fe97
rajctg <39931753+jashim2018@users.noreply.github.com>
2020-01-17 18:24:49 +0600

    Create dark1.8.sh

---
+++ dark1.8.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/jashim2018/dark1.8
+git clone https://github.com/jashim2018/dark1.8A
+git clone https://github.com/jashim2018/dark1.8eng
+git clone https://github.com/jashim2018/dark1.8iemfast
