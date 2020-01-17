b4eaa44971b9801db1e7dd8381e0cfc629268220
rajctg <39931753+jashim2018@users.noreply.github.com>
2020-01-17 18:43:24 +0600

    Create darkp.sh

---
+++ darkp.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/jashim2018/darkp1
+git clone https://github.com/jashim2018/darkp2
+git clone https://github.com/jashim2018/darkp3
+git clone https://github.com/jashim2018/darkp4
+git clone https://github.com/jashim2018/darkp5
+git clone https://github.com/jashim2018/darkp6
+git clone https://github.com/jashim2018/darkp7
+git clone https://github.com/jashim2018/darkp8
