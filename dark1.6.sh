56819727df93db772f62e04dd1e5eed406d31e40
rajctg <39931753+jashim2018@users.noreply.github.com>
2020-01-18 01:01:33 +0600

    Update dark1.6.sh

--- dark1.6.sh
+++ dark1.6.sh
@@ -1,4 +1,1 @@
-git clone https://github.com/jashim2018/dark1.6
-git clone https://github.com/jashim2018/dark1.6a
-git clone https://github.com/jashim2018/dark1.6b
-git clone https://github.com/jashim2018/dark1.6c
+
